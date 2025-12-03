6d2e8079529f3d18738cfa8aeb4178461ddcbc63
ichiil <bloodgamer776@gmail.com>
2025-12-03 16:31:52 +0700

    Подсчёт правильных ответов

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geoquiz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,18 +16,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.geoquiz.ui.theme.GeoQuizTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,6 +64,7 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun QuizScreen() {
+
         var index by remember { mutableStateOf(0) }
         val q = questions[index]
         Column(
@@ -71,6 +78,7 @@ class MainActivity : ComponentActivity() {
             Text(q.text, style = MaterialTheme.typography.titleLarge)
             Spacer(Modifier.height(60.dp))
 
+
             var answered by remember { mutableStateOf(false) }
             var score by remember { mutableStateOf(0) }
             if (!answered) {
@@ -78,14 +86,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Button(onClick = {
+                    Button(onClick = { // true
                         if (q.answer) score++
                         answered = true
                     }) {
                         Text("TRUE")
                     }
 
-                    Button(onClick = {
+                    Button(onClick = { // false
                         if (!q.answer) score++
                         answered = true
                     }) {
@@ -94,27 +102,36 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
+
             Spacer(Modifier.height(40.dp))
             var quizFinished by remember { mutableStateOf(false) }
-            if(!quizFinished) {
+            val snackbarHostState = remember { SnackbarHostState() }
+            if (!quizFinished) {
                 Button(
                     onClick = {
                         if (index < questions.lastIndex) {
                             index++
                             answered = false
-                        }
-                        if (index == questions.lastIndex) {
+                        } else {
+                            // после последнего вопроса
                             quizFinished = true
                         }
                     },
                     enabled = answered,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text("NEXT")
+                    Text(if (index < questions.lastIndex) "NEXT" else "FINISH")
                 }
             }
-        }
 
+
+            if (quizFinished) {
+                LaunchedEffect(Unit) {
+                    snackbarHostState.showSnackbar("Result: $score correct answers")
+                }
+            }
+            SnackbarHost(hostState = snackbarHostState)
+        }
     }
 }
 
